9937c34d8859d4981a81bc457dd5113d7dc1bdfe
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-31 13:40:59 +0530

    initialcommit

--- HashTables/HashTables/Program.cs
+++ HashTables/HashTables/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Data Struchure Hash Tables");
+Console.WriteLine("Welcome to Data Struchure Hash Tables and Binary Search Table");
